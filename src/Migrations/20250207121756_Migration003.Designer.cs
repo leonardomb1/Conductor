d73d03bf950cb41a455b28671b5edd950a9a4367
leonardomb1 <lmbaudin2013@gmail.com>
2025-02-07 09:18:43 -0300

    Fix: Update DestinationId to be nullable in EXTRACTIONS table and adjust foreign key constraints

--- src/Migrations/20250207121756_Migration003.Designer.cs
+++ src/Migrations/20250207121756_Migration003.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Conductor.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Conductor.Migrations
 {
     [DbContext(typeof(EfContext))]
-    partial class EfContextModelSnapshot : ModelSnapshot
+    [Migration("20250207121756_Migration003")]
+    partial class Migration003
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
