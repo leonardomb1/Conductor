51960ce6fcfcf0ceefa94a70ba464b2cabe4d7f1
leonardomb1 <lmbaudin2013@gmail.com>
2025-02-10 21:51:49 -0300

    Feat: Add IgnoreColumns property to EXTRACTIONS table for dynamic column handling

---
+++ src/Migrations/20250211005139_Migration0002.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Conductor.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration0002 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IgnoreColumns",
+                table: "EXTRACTIONS",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IgnoreColumns",
+                table: "EXTRACTIONS");
+        }
+    }
+}
